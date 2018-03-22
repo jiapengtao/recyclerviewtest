9bc3beb8f06d1e0c19be9f367bca23f3d33f5007
jiapengtao <1149846858@qq.com>
2018-03-22 23:02:34 +0800

    阿斯蒂芬
    阿斯蒂芬

--- recyclerviewtest/src/main/java/com/jiapt/recyclerviewtest/MainActivity.java
+++ recyclerviewtest/src/main/java/com/jiapt/recyclerviewtest/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         rvTest = findViewById(R.id.rv_test);
         for (int i = 0; i < 100; i++) {
             DataBean dataBean = new DataBean();
-            dataBean.setText("第" + (i + 1) + "条数据");
+            dataBean.setText("第" + (i + 1) + "条数据"); 
             data.add(dataBean);
         }
 
